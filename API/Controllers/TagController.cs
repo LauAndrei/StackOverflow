3b854d9846600ac4e056fd8ef0660eec1e32fdf1
LauAndrei <lauran.andrei@gmail.com>
2023-03-11 19:50:24 +0200

    Initial commit - set up EF Core and Project Architecture

---
+++ API/Controllers/TagController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TagController : ControllerBase
+{
+    [HttpGet]
+    [Route("GetAllTags")]
+    public string GetTags()
+    {
+        return "This will be a list of tags";
+    }
+
+}
---
+++ Infrastructure/Repositories/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    private DbSet<Tag> Tags { get; set; }
+}
