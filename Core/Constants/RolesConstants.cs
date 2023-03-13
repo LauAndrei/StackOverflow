3a111d47ef737d0676818ceeead4d0ceb7d356e0
LauAndrei <lauran.andrei@gmail.com>
2023-03-13 18:06:32 +0200

    Added Constants n Role Seed

---
+++ Core/Constants/RolesConstants.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Core.Constants;
+
+public static class ROLES_CONSTANTS
+{
+    public static class ROLES
+    {
+        public const string STANDARD = "Standard";
+        public const string MODERATOR = "Moderator";
+    }
+}
---
+++ Infrastructure/Repositories/SeedData/RoleSeed.cs
@@ -0,0 +1,19 @@
+﻿using Core.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Repositories.SeedData;
+
+public class RoleSeed
+{
+    public static async Task SeedRolesAsync(RoleManager<IdentityRole<int>> roleManager)
+    {
+        if (!roleManager.Roles.Any())
+        {
+            var standardRole = new IdentityRole<int>(ROLES_CONSTANTS.ROLES.STANDARD);
+            var moderatorRole = new IdentityRole<int>(ROLES_CONSTANTS.ROLES.MODERATOR);
+
+            await roleManager.CreateAsync(standardRole);
+            await roleManager.CreateAsync(moderatorRole);
+        }
+    }
+}
